f475867e4f27cd2f26ed239a137f0fc92c100009
James King <metapyziks@gmail.com>
2013-10-07 19:21:35 +0100

    Worked on article displays on home page

--- JonsFirstThing/HomePage.cs
+++ JonsFirstThing/HomePage.cs
@@ -11,13 +11,43 @@ namespace JonsFirstThing
     [ServletURL("/")]
     class HomePage : Page
     {
+        private static int IndexOfOrLength(String str, String value)
+        {
+            int index = str.IndexOf(value);
+            if (index == -1) return str.Length;
+            return index;
+        }
+
+        private static String GetFirstParagraph(String content)
+        {
+            int end = IndexOfOrLength(content, "\n\n");
+            end = Math.Min(end, IndexOfOrLength(content, "\r\n\r\n"));
+            return content.Substring(0, end);
+        }
+
         protected override void Content()
         {
-            var articles = Database.SelectAll<Article>();
-            foreach (var article in articles)
+            var articles = Database.SelectAll<Article>().OrderByDescending(x => x.Posted);
+            foreach (var article in articles.Take(4))
             {
                 Write(
+                    Tag("h2")(article.Title),
+                    Tag("p")(
+                        Dyn(() => {
+                            var firstParagraph = GetFirstParagraph(article.Content);
+                            Write(firstParagraph);
 
+                            if (firstParagraph.Length < article.Content.Length) {
+                                Write(Ln, Ln, Tag("a", href => Format("/article?id={0}", article.ID))("Read&nbsp;More&nbsp;>>"));
+                            }
+                        })
+                    ),
+                    Dyn(() => {
+                        if (article != articles.Last()) {
+                            Write(Tag("div", @class => "hrule"));
+                        }
+                    })
+                );
             }
         }
     }
